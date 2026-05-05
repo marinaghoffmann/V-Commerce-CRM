469c6304172597fa0fb45e1d3bf9c0b32373cb7e
vlsmcin <viniciuslimamelo7@gmail.com>
2026-05-05 14:45:40 -0300

    feat: Create router modularization

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+import uvicorn
+from app.routers.router import router
 
 app = FastAPI(title="404HexaNotFound API")
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
